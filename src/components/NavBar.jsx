fcdad6dda745f065966b9df48f16663f02a9ec48
nini-btw <mohamed01112002@gmail.com>
2024-10-01 21:13:22 +0100

    second commit still strugling with the nav bar and the dark mode icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,25 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+import '../stylesheets/navstyle.css'
 
 function NavBar(){
     return(
         <>
-        <Navbar expand="lg" className="bg-body-tertiary px-2" fixed="top">
-            <Link to="/" className='navbar-brand'>Logo</Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link>Home</Nav.Link>
-                <Nav.Link>About Me</Nav.Link>
-                <Nav.Link>Projects</Nav.Link>
-                <Nav.Link>Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <div className="nav position-relative">
+          <Navbar expand="lg" className="bg-body-tertiary px-5 d-flex justify-content-between" fixed="top" id='nav'>
+              <Link to="/" className='navbar-brand'>Logo</Link>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse className='flex-grow-0' id="basic-navbar-nav flex-grow-1">
+                <Nav className="me-auto">
+                  <Nav.Link>Home</Nav.Link>
+                  <Nav.Link>About Me</Nav.Link>
+                  <Nav.Link>Projects</Nav.Link>
+                  <Nav.Link>Contact</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+        </div>
         </>
     )
 }
