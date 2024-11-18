c50c553602d917c33144f54c7541fc95ea47ba4a
nini-btw <mohamed01112002@gmail.com>
2024-11-18 23:52:33 +0100

    start the porject section

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,53 +1,58 @@
-import '../stylesheets/aboutmeS.sass'
-import { Container, Row, Col } from 'react-bootstrap';
+import "../stylesheets/aboutmeS.sass";
+import { Container, Row, Col } from "react-bootstrap";
 
-
-function Aboutme(){
-    return (
-        <>
-            <Container className='aboutme d-flex flex-dir align-items-center flex-column'>
-                <header className='text-center'>
-                <h2 className="heading heading-sec heading-sec__mb-med">
-                    <span className="heading-sec__main">About Me</span>
-                    <span className="heading-sec__sub">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis
-                        tempora explicabo quae quod deserunt eius sapiente
-                    </span>
-                </h2>
-                </header>
-                <section className='d-flex justify-content-center align-items-center'>
-                    <Row>
-                        <Col md={6} xs={12} className='skills'>
-                            <div className="">
-                                <h3>Get to Know me!</h3>
-                                <p>Hello! I&apos;m <strong>Mohammed</strong>, a <strong>MERN Stack Developer</strong> based in <strong>Oran</strong> with a <strong>Bachelor’s degree in Computer Science</strong>. I’m passionate about building dynamic, user-focused applications and bringing clients’ visions to life through <strong>scalable and efficient software solutions</strong>. Let’s work together to turn your ideas into reality!</p>
-
-                            </div>
-                            <div className="cv">
-                                CV
-                            </div>
-                        </Col>
-                        <Col md={6} xs={12}>
-                            <div className=" description">
-                                <h3>My Skills</h3>
-                                <div className="elements d-flex flex-wrap">
-                                    <div className="element">HTML</div>
-                                    <div className="element">CSS</div>
-                                    <div className="element">JavaScript</div>
-                                    <div className="element">React</div>
-                                    <div className="element">REDUX</div>
-                                    <div className="element">SASS</div>
-                                    <div className="element">GIT</div>
-                                    <div className="element">NODE</div>
-                                    <div className="element">EXPRESS</div>
-                                    <div className="element">MONGO DB</div>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </section>
-            </Container>
-        </>
-    )
+function Aboutme() {
+  return (
+    <>
+      <Container className="aboutme d-flex flex-dir align-items-center flex-column">
+        <header className="text-center">
+          <h2 className="heading heading-sec heading-sec__mb-med">
+            <span className="heading-sec__main">About Me</span>
+            <span className="heading-sec__sub">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+              facilis tempora explicabo quae quod deserunt eius sapiente
+            </span>
+          </h2>
+        </header>
+        <section className="d-flex justify-content-center align-items-center">
+          <Row>
+            <Col md={6} xs={12} className="skills">
+              <div className="">
+                <h3>Get to Know me!</h3>
+                <p>
+                  Hello! I&apos;m <strong>Mohammed</strong>, a{" "}
+                  <strong>MERN Stack Developer</strong> based in{" "}
+                  <strong>Oran</strong> with a{" "}
+                  <strong>Bachelor’s degree in Computer Science</strong>. I’m
+                  passionate about building dynamic, user-focused applications
+                  and bringing clients’ visions to life through{" "}
+                  <strong>scalable and efficient software solutions</strong>.
+                  Let’s work together to turn your ideas into reality!
+                </p>
+              </div>
+              <div className="cv">CV</div>
+            </Col>
+            <Col md={6} xs={12}>
+              <div className=" description">
+                <h3>My Skills</h3>
+                <div className="elements d-flex flex-wrap">
+                  <div className="element">HTML</div>
+                  <div className="element">CSS</div>
+                  <div className="element">JavaScript</div>
+                  <div className="element">React</div>
+                  <div className="element">REDUX</div>
+                  <div className="element">SASS</div>
+                  <div className="element">GIT</div>
+                  <div className="element">NODE</div>
+                  <div className="element">EXPRESS</div>
+                  <div className="element">MONGO DB</div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </section>
+      </Container>
+    </>
+  );
 }
 export default Aboutme;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,46 @@
-function Project(){
-    return (
-        <>
-        <h1>Project</h1>
-        <div className="box"></div>
-        </>
-    )
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "../stylesheets/projectS.sass";
+
+function Project() {
+  return (
+    <>
+      <header className="text-center">
+        <h2 className="heading heading-sec heading-sec__mb-med">
+          <span className="heading-sec__main">About Me</span>
+          <span className="heading-sec__sub">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis
+            tempora explicabo quae quod deserunt eius sapiente
+          </span>
+        </h2>
+      </header>
+      <div className="section swiper-container d-flex align-items-center justify-content-center">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          navigation
+          autoplay={{ delay: 3000 }}
+          loop
+          pagination={{ clickable: true }}
+          direction="horizontal"
+        >
+          <SwiperSlide className="d-flex align-items-center justify-content-center">
+            Slide 1
+          </SwiperSlide>
+          <SwiperSlide className="d-flex align-items-center justify-content-center">
+            Slide 2
+          </SwiperSlide>
+          <SwiperSlide className="d-flex align-items-center justify-content-center">
+            Slide 3
+          </SwiperSlide>
+          <SwiperSlide className="d-flex align-items-center justify-content-center">
+            Slide 4
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 }
 export default Project;
